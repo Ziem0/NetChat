54a66c9f96ec7a487377c9d3770fe1522e470e0c
Ziem0 <andrzejewski.ziemo@gmail.com>
2019-02-28 19:50:03 +0100

    minor issues fixed

--- src/main/java/com/networkSerialization/MultiApp/ChatterM.java
+++ src/main/java/com/networkSerialization/MultiApp/ChatterM.java
@@ -1,20 +1,10 @@
 package com.networkSerialization.MultiApp;
 
-import java.io.*;
-import java.net.Socket;
-
 public abstract class ChatterM {
 
 	String name;
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public ChatterM(String name) {
 		this.name = name;
-	}
-
-	public void chat(Socket client) throws IOException {
-
-
-
 	}
 }
